956d89503c7e3bc701bbb42af94b890e3039c3be
day070 <hidayatsalmanan25@gmail.com>
2025-07-15 13:04:05 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace anjay
+{
+    class programram
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a Number ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Select operations");
+            string operations = Console.ReadLine();
+
+
+            Console.WriteLine("Enter a Number ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            if (operations == "+") {
+                Console.WriteLine(num1 + num2);
+            }if (operations == "*"){
+                Console.WriteLine(num1 * num2);                
+            }if (operations == "/")
+            {
+                Console.WriteLine(num1 / num2);
+            }
+            else
+            {
+                Console.WriteLine("Maaf format operasi anda tidak benar");
+            }
+        }
+    }
+}
